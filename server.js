151a7ca484ad56d0c403a838f695b64c7ac64f5c
Arttu <arttu.nevalainen@granite.fi>
2018-09-19 15:58:46 +0300

    Almost production ready.

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 
 
 const axios = require('axios');
-
 const Bot = require('node-telegram-bot-api');
-const request = require('request');
+const token = "625526793:AAE5fbK2FNeYyoZV_MP_wn-S5Qhj2zS0-7Y";
 
-const url = 'https://launchlibrary.net/1.3/launch';
-const trigger = 'I want to travel!';
-const token = '625526793:AAE75GVGYHw1Q2p_31toKXpmI9EC1avbHhM';
 
 const bot = new Bot(token, {polling: true});
 
+
 bot.on('message', (msg) => {
-    getData().then((data) => {
-        console.log(data);
-    });
-    console.log(msg.text);
-    bot.sendMessage(msg.chat.id, alkot[0].alko + " " + alkot[0].lkm);
+    getAlko(msg.text).then((alko) => {
+        bot.sendMessage(msg.chat.id, alko.alko + " " + alko.lkm);
+    })
 });
 
-
 let alkot = [];
+getData();
 
 function getData() {
     axios.get('https://www.alko.fi/INTERSHOP/web/WFS/Alko-OnlineShop-Site/fi_FI/-/EUR/ViewProduct-Include?SKU=319027').then((res) => {
-    
+
         let wasd = res.data.split("</a>");
         wasd.forEach(line => {
             line = line.split(/<[^>]+>/g);
@@ -32,24 +27,46 @@ function getData() {
             line[4] = line[4].replace(/&Auml;/g, 'Ä');
             line[4] = line[4].replace(/&ouml;/g, 'ö');
             line[4] = line[4].replace(/&Ouml;/g, 'Ö');
-    
+            line[4] = line[4].replace("Alko ", "");
+
             alkot.push({ alko: line[4], lkm: line[6] });
         });
-    
-        return "kek";
+
+        alkot.shift();
+        alkot.shift();
+
+        console.log("Gambinat kassissa!");
     }).catch(function (error) {
         console.log(error);
     });
 }
 
+async function getAlko(alkoname) {
+    alkoname = alkoname.split(" ");
+    let found = null;
 
-
-function getAlko(body) {
     alkot.forEach(alko => {
-        if(alko.alko === body.alko) {
-            return alko;
+        console.log(alko);
+        let splittedalko = alko.alko.split(" ");
+
+        if(splittedalko[0] === alkoname[1]) {
+            console.log("found!");
+            found = alko;
+        }
+        else if(splittedalko[1] === alkoname[1]) {
+            console.log("found!");
+            found = alko;
+        }
+        else if(splittedalko[2] === alkoname[1]) {
+            console.log("found!");
+            found = alko;
         }
     });
 
-    return 'no such alko';
+    if(found != null) {
+        return found;
+    }
+    else {
+        return {alko: "Alko not found.", lkm: "" };
+    }
 }
